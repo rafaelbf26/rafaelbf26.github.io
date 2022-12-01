cef75a2f383a2cb83cc82112b6616cf6c6b875d8
Rafael Bis <rafaelbisferreira@gmail.com>
2022-12-01 18:17:53 -0300

    applying changes to add geolocation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'adventures';
+  title = 'Lucci & Banguela Adventures';
+  public latitude = 0;
+  public longitude = 0;
+  public MAP_MODE = 'place';
+  public API_KEY = 'AIzaSyB07uRZ1XDqVSH0qUjkLlTom5x78N2Vdkw';
+  public PARAMETERS = '';
+  public bigscreenRef = 'https://maps.google.com/maps?q='+this.latitude+','+this.longitude+'&hl=es;z=14&amp;output=embed';
+
+  ngOnInit(){
+    navigator.geolocation.getCurrentPosition(pos => {
+      this.latitude = pos.coords.latitude;
+      this.longitude = pos.coords.longitude;
+      this.PARAMETERS = '';
+      let src = "https://www.google.com/maps/embed/v1/place?key=" + this.API_KEY + "%20&q=" + this.latitude + ',' + this.longitude;
+      let frame: any = document.getElementById('test');
+      if (frame != null) frame.src = src;
+    },pos => pos);
+
+    navigator.geolocation.getCurrentPosition(pos => {
+      this.latitude = pos.coords.latitude;
+      this.longitude = pos.coords.longitude;
+    },pos => pos);
+  }
+
 }
